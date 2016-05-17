4e0f1b7d1c8f32f44275bf60fbbd03f06bf02321
ThePsionic <gh@thepsionic.com>
2016-05-17 23:06:30 +0200

    Allow for multiple links per message

--- wikilinker.js
+++ wikilinker.js
@@ -4,21 +4,36 @@ var config = require("./config.json");
 var bot = new Discord.Client();
 
 bot.on("message", function(msg) {
-    if (msg.cleanContent.search(/\[\[.+\]\]/g) !== -1) {
+    if (msg.cleanContent.search(/\[\[([^\]\|]+)(?:|[^\]]+)?\]\]/g) !== -1) {
         console.log("Link found!");
-        name = msg.cleanContent.replace(/\[\[(.+)\]\]/g, "$1");
-        name = name.replace(/ /g, "_");
-        if (name.search(/#/g) !== -1) {
-            bot.sendMessage(msg.channel, "\<http://rs.wikia.com/"+name+"\>");
-        } else if (name.search(/\?action=\w+/g) !== -1) {
-            bot.sendMessage(msg.channel, "\<http://rs.wikia.com/"+name+"\>");
-        } else {
-            if (name.search(/\?/g) !== -1) {
-                name = name.replace(/\?/g, "%63");
+        cleaned = msg.cleanContent.replace(/\u200B/g, "");
+        name = cleaned.replace(/.*?\[\[([^\]\|]+)(?:|[^\]]+)?\]\]/g, "$1\u200B");
+        allLinks = name.split("\u200B");
+        console.log(allLinks);
+        allLinks.pop();
+        replyString = "**Wiki links detected:**";
+        for (item in allLinks) {
+            allLinks[item] = allLinks[item].trim();
+            console.log(allLinks[item]);
+            replyString += "\n"
+            allLinks[item] = allLinks[item].replace(/ /g, "_");
+            if (allLinks[item].search(/#/g) !== -1) {
+                replyString += "\<http://rs.wikia.com/" + allLinks[item] + "\>";
+            } else if (allLinks[item].search(/\?action=\w+/g) !== -1) {
+                replyString += "\<http://rs.wikia.com/" + allLinks[item] + "\>";
+            } else {
+                if (allLinks[item].search(/\?/g) !== -1) {
+                    allLinks[item] = allLinks[item].replace(/\?/g, "%63");
+                }
+                replyString += "\<http://rs.wikia.com/" + allLinks[item] + "\?action=view\>";
             }
-            bot.sendMessage(msg.channel, "\<http://rs.wikia.com/"+name+"?action=view\>");
         }
+        bot.sendMessage(msg.channel, replyString);
     }
+});
+
+bot.on("disconnected", function() {
+    bot.loginWithToken(config.token);
 });
 
 bot.loginWithToken(config.token);
