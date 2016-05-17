1444d66f319ff0cb644283064975d01446085843
ThePsionic <gh@thepsionic.com>
2016-05-18 00:14:23 +0200

    Use Wikia API to predict closest match, fixes #3

--- wikilinker.js
+++ wikilinker.js
@@ -1,39 +1,48 @@
 var Discord = require("discord.js");
+var request = require("request");
 var config = require("./config.json");
 
 var bot = new Discord.Client();
 
 bot.on("message", function(msg) {
     if (msg.cleanContent.search(/\[\[([^\]\|]+)(?:|[^\]]+)?\]\]/g) !== -1) {
-        console.log("Link found!");
         cleaned = msg.cleanContent.replace(/\u200B/g, "");
         name = cleaned.replace(/.*?\[\[([^\]\|]+)(?:|[^\]]+)?\]\]/g, "$1\u200B");
         allLinks = name.split("\u200B");
-        console.log(allLinks);
+
         allLinks.pop();
         replyString = "**Wiki links detected:**";
+
         for (item in allLinks) {
             allLinks[item] = allLinks[item].trim();
-            console.log(allLinks[item]);
-            replyString += "\n"
-            allLinks[item] = allLinks[item].replace(/ /g, "_");
-            if (allLinks[item].search(/#/g) !== -1) {
-                replyString += "\<http://rs.wikia.com/" + allLinks[item] + "\>";
-            } else if (allLinks[item].search(/\?action=\w+/g) !== -1) {
-                replyString += "\<http://rs.wikia.com/" + allLinks[item] + "\>";
-            } else {
-                if (allLinks[item].search(/\?/g) !== -1) {
-                    allLinks[item] = allLinks[item].replace(/\?/g, "%63");
-                }
-                replyString += "\<http://rs.wikia.com/" + allLinks[item] + "\?action=view\>";
-            }
+
+            reqAPI(allLinks[item], function() {
+                replyString += "\n<" + this + ">";
+            })
         }
-        bot.sendMessage(msg.channel, replyString);
+        setTimeout(function() {bot.sendMessage(msg.channel, replyString);}, 1000);
     }
 });
 
 bot.on("disconnected", function() {
     bot.loginWithToken(config.token);
 });
+
+function reqAPI(requestname, callback) {
+    request({
+        method: "GET",
+        uri: "http://runescape.wikia.com/api/v1/Search/List/?query="+requestname+"&limit=1&namespaces=0%2C14",
+        json: true
+    }, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log("First item: " + body.items[0]);
+            callback.call(body.items[0].url);
+        } else if (error) {
+            console.log("Error: " + error);
+        } else {
+            console.log("Response code: " + response.statusCode);
+        }
+    });
+}
 
 bot.loginWithToken(config.token);
